abf41f758ffae0f1138d4fa83ccca621db3dc8de
Luis Ascencio <luismiguelascencio@gmail.com>
2019-08-25 18:58:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, eventName) {
+  let wc = [];
+  for (let i = 0; i < name.length; i++) {
+    wc.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+  }
+  return wc;
+}
+
+function countdown(number) {
+  let count = 0;
+  while (count <= number) {
+    console.log(count++);
+  }
+}
